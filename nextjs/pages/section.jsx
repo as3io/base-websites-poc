7d15e63814219ce047b920264eef4c8614ac9c89
Jacob Bare <jacob@limit0.io>
2018-09-11 10:55:56 -0500

    Create faux section page

---
+++ nextjs/pages/section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h1>Section</h1>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
