746722b8688d0f78cc3e07bb46ea016558165120
Jacob Bare <jacob@limit0.io>
2018-09-11 09:59:20 -0500

    Wrap the entire app with apollo

---
+++ nextjs/pages/_app.jsx
@@ -0,0 +1,27 @@
+import App, { Container } from 'next/app';
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import withApollo from '../apollo/WithApollo';
+
+class WebsiteApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return pageProps;
+  }
+
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withApollo(WebsiteApp);
