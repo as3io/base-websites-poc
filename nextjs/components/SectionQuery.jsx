86b6f263e29bd2e7bbb34fbd3310c482457ba76e
Jacob Bare <jacob@limit0.io>
2018-09-11 09:59:43 -0500

    Create simple section query component

---
+++ nextjs/components/SectionQuery.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+
+import query from '../gql/blocks/section-query.graphql';
+
+const SectionQuery = ({
+  sectionId,
+  first,
+  after,
+  children,
+}) => {
+  const pagination = { first, after };
+  const input = { sectionId, pagination };
+  return (
+    <Fragment>
+      <Query query={query} variables={{ input }}>
+        {({ loading, error, data }) => (children({ loading, error, data }))}
+      </Query>
+    </Fragment>
+  );
+};
+
+SectionQuery.defaultProps = {
+  after: null,
+  children: () => {},
+  first: 5,
+};
+
+SectionQuery.propTypes = {
+  sectionId: PropTypes.number.isRequired,
+  after: PropTypes.string,
+  children: PropTypes.func,
+  first: PropTypes.number,
+};
+
+export default SectionQuery;
