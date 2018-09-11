2ef287c8b636416a97947db630e3559998042dc5
Jacob Bare <jacob@limit0.io>
2018-09-11 10:00:26 -0500

    Add test section query blocks to “home” page

--- nextjs/pages/index.jsx
+++ nextjs/pages/index.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import SectionQuery from '../components/SectionQuery';
 
 export default () => (
   <div>
-    <h1>Hello, world!</h1>
+    <h1>Home</h1>
+    <SectionQuery sectionId={56374}>
+      {({ loading, error }) => {
+        if (loading) return <span>Loading...</span>;
+        if (error) {
+          return (
+            <span>
+              Error
+              {' '}
+              {error.message}
+            </span>
+          );
+        }
+
+        return (
+          <p>
+            Loaded
+            {' '}
+            56374
+          </p>
+        );
+      }}
+    </SectionQuery>
+
+    <SectionQuery sectionId={59622}>
+      {({ loading, error }) => {
+        if (loading) return <span>Loading...</span>;
+        if (error) {
+          return (
+            <span>
+              Error
+              {' '}
+              {error.message}
+            </span>
+          );
+        }
+
+        return (
+          <p>
+            Loaded
+            {' '}
+            59622
+          </p>
+        );
+      }}
+    </SectionQuery>
   </div>
 );
