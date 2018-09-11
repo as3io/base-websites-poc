8c003e1c05e73dc514963a2980af97d96feb3ade
Jacob Bare <jacob@limit0.io>
2018-09-11 15:00:49 -0500

    Create initial content page

---
+++ nextjs/pages/content.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import DefaultLayout from '../layouts/Default';
+
+import contentPage from '../gql/queries/content-page.graphql';
+
+const createMarkup = html => ({ __html: html });
+
+const ContentPage = ({ id }) => {
+  const input = { id };
+  return (
+    <DefaultLayout>
+      <Query query={contentPage} variables={{ input }}>
+        {({ loading, error, data }) => {
+          if (loading) return <span>Loading...</span>;
+          if (error) return <span>{error.message}</span>;
+          const { platformContent } = data;
+          return (
+            <article>
+              <h1>{platformContent.name}</h1>
+              <h5>{platformContent.teaser}</h5>
+              <hr />
+              {/* eslint-disable-next-line react/no-danger */}
+              <div dangerouslySetInnerHTML={createMarkup(platformContent.body)} />
+            </article>
+          );
+        }}
+      </Query>
+    </DefaultLayout>
+  );
+};
+
+ContentPage.getInitialProps = async ({ query }) => {
+  const { id } = query;
+  return { id };
+};
+
+ContentPage.propTypes = {
+  id: PropTypes.string.isRequired,
+};
+
+export default ContentPage;
