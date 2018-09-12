65b5b832041e185534b912608e835b9d86247205
Jacob Bare <jacob@limit0.io>
2018-09-12 07:58:19 -0500

    Add TENANT_KEY and GRAPHQL_TOKEN

--- nextjs/src/server/env.js
+++ nextjs/src/server/env.js
@@ -12,7 +12,19 @@ const url = makeValidator((v) => {
   throw new Error('Expected a valid URL with http or https');
 });
 
+const nonemptystr = makeValidator((v) => {
+  const err = new Error('Expected a non-empty string');
+  if (v === undefined || v === null || v === '') {
+    throw err;
+  }
+  const trimmed = String(v).trim();
+  if (!trimmed) throw err;
+  return trimmed;
+});
+
 module.exports = cleanEnv(process.env, {
   PORT: port({ desc: 'The port that express will run on.', default: 3005 }),
-  GRAPHQL_URI: url({ desc: 'The GraphQL URI for proxying/stitching API requests.' }),
+  GRAPHQL_URL: url({ desc: 'The GraphQL URI for proxying/stitching API requests.' }),
+  TENANT_KEY: nonemptystr({ desc: 'The Base4 tenant key to connect to.' }),
+  GRAPHQL_TOKEN: nonemptystr({ desc: 'The GraphQL API token.' }),
 });
