504524be37f5826ebd0405bebbe39b1630196382
Jacob Bare <jacob@limit0.io>
2018-09-13 14:12:49 -0500

    Remove consoles

--- nextjs/src/lib/check-content.js
+++ nextjs/src/lib/check-content.js
@@ -3,10 +3,8 @@ import redirect from './redirect';
 export default (content, { res }) => {
   if (res) {
     // Run on server only.
-    console.info('checking content...');
     const { redirectTo } = content;
     if (redirectTo) {
-      console.info('redirecting externally...');
       redirect(res, redirectTo);
     }
   }
