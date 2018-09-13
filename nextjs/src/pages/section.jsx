2e424c71f9883679a4c6450dd1646094d849f612
Jacob Bare <jacob@limit0.io>
2018-09-13 14:18:28 -0500

    Update section page to query within getinitialprops

--- nextjs/src/pages/section.jsx
+++ nextjs/src/pages/section.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
 import DefaultLayout from '../layouts/Default';
 import ContentList from '../components/Content/List';
+import is404 from '../lib/is-404';
 
 import sectionPage from '../gql/queries/section-page.graphql';
 
-const SectionPage = ({ alias }) => {
-  const input = { alias };
-  return (
-    <DefaultLayout>
-      <Query query={sectionPage} variables={{ input }}>
-        {({ loading, error, data }) => {
-          if (loading) return <span>Loading...</span>;
-          if (error) return <span>{error.message}</span>;
-          const { websiteSectionAlias } = data;
-          return (
-            <section>
-              <h1>{websiteSectionAlias.name}</h1>
-              <ContentList sectionId={websiteSectionAlias.id} />
-            </section>
-          );
-        }}
-      </Query>
-    </DefaultLayout>
-  );
-};
+const SectionPage = ({ section }) => (
+  <DefaultLayout>
+    <section>
+      <h1>{section.name}</h1>
+      <ContentList sectionId={section.id} />
+    </section>
+  </DefaultLayout>
+);
 
-SectionPage.getInitialProps = async ({ query }) => {
+SectionPage.getInitialProps = async (ctx) => {
+  const { query, apollo, res } = ctx;
   const { alias } = query;
-  return { alias };
+  const input = { alias };
+  const variables = { input };
+
+  try {
+    // @todo Should this be a watch query?
+    const { data } = await apollo.query({ query: sectionPage, variables });
+    const { websiteSectionAlias } = data;
+    return { section: websiteSectionAlias };
+  } catch (e) {
+    if (res) res.statusCode = 500;
+    if (is404(e)) {
+      e.code = 'ENOENT';
+      if (res) res.statusCode = 404;
+    }
+    throw e;
+  }
 };
 
 SectionPage.propTypes = {
-  alias: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  section: PropTypes.object.isRequired,
 };
 
 export default SectionPage;
