b777e1992fb48d4afde1bcf7dc8078142b1babb2
Jacob Bare <jacob@limit0.io>
2018-09-14 07:56:21 -0500

    Rework redirect to support asPath

--- nextjs/src/lib/redirect.js
+++ nextjs/src/lib/redirect.js
@@ -1,12 +1,18 @@
 import Router from 'next/router';
 
-export default (res, target, code = 301) => {
+/**
+ * @param {?object} res The Express response object.
+ * @param {string} href The redirect href.
+ * @param {?string} asPath The redirect as path.
+ * @param {number} [code=301] The redirect response code.
+ */
+export default (res, href, asPath, code = 301) => {
   if (res) {
     // Server-side only.
-    res.writeHead(code, { Location: target });
+    res.writeHead(code, { Location: asPath || href });
     res.end();
   } else {
-    // Client-side. Pretend this never happened.
-    Router.replace(target);
+    // Client-side.
+    Router.replace(href, asPath);
   }
 };
