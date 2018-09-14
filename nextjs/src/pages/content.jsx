008a583ccc7882659fc7a439f524d49fb5d230bf
Jacob Bare <jacob@limit0.io>
2018-09-14 08:40:50 -0500

    Handle 404s on content similar to section

--- nextjs/src/pages/content.jsx
+++ nextjs/src/pages/content.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import DefaultLayout from '../layouts/Default';
 import checkContent from '../lib/check-content';
 import createMarkup from '../lib/create-markup';
-import is404 from '../lib/is-404';
+import errors from '../lib/errors';
 import RelCanonical from '../components/Content/RelCanonical';
 
 import contentPage from '../gql/queries/content-page.graphql';
@@ -25,7 +25,6 @@ ContentPage.getInitialProps = async (ctx) => {
   const {
     query,
     apollo,
-    res,
     req,
   } = ctx;
   const { id } = query;
@@ -35,23 +34,17 @@ ContentPage.getInitialProps = async (ctx) => {
   // @todo Make this a HOC for all pages.
   const origin = req ? `${req.protocol}://${req.get('host')}` : `${window.location.protocol}//${window.location.host}`;
 
-  try {
-    // @todo Should this be a watch query?
-    // @todo Should page-level queries be consolidated for re-use across all pages?
-    const { data } = await apollo.query({ query: contentPage, variables });
-    const { platformContent } = data;
-
-    // Check content for internal/external redirects, etc.
-    checkContent(platformContent, ctx);
-    return { content: platformContent, origin };
-  } catch (e) {
-    if (res) res.statusCode = 500;
-    if (is404(e)) {
-      e.code = 'ENOENT';
-      if (res) res.statusCode = 404;
-    }
-    throw e;
+  // @todo Should this be a watch query?
+  // @todo Should page-level queries be consolidated for re-use across all pages?
+  // @todo Do we still need try/catch?
+  const { data } = await apollo.query({ query: contentPage, variables });
+  const { platformContent } = data;
+  if (!platformContent) {
+    throw errors.notFound();
   }
+  // Check content for internal/external redirects, etc.
+  checkContent(platformContent, ctx);
+  return { content: platformContent, origin };
 };
 
 ContentPage.propTypes = {
