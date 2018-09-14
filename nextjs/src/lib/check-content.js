056015c375f576e433770deff1762ee02d5e4c6d
Jacob Bare <jacob@limit0.io>
2018-09-14 07:56:44 -0500

    Rework content redirects to support asPath

--- nextjs/src/lib/check-content.js
+++ nextjs/src/lib/check-content.js
@@ -1,12 +1,20 @@
 import redirect from './redirect';
 
+/**
+ * @param {object} content The content object from GraphQL.
+ * @param {object} ctx The NextJS context object.
+ * @param {?object} ctx.res The Experss response object.
+ * @param {string} ctx.asPath The Next Router `as` path.
+ */
 export default (content, { res, asPath }) => {
-  if (res) {
-    const { redirectTo, canonicalPath } = content;
-    if (redirectTo) {
-      redirect(res, redirectTo);
-    } else if (canonicalPath !== asPath) {
-      redirect(res, canonicalPath);
-    }
+  const { id, redirectTo, canonicalPath } = content;
+  if (redirectTo) {
+    redirect(res, redirectTo);
+    return true;
   }
+  if (canonicalPath !== asPath) {
+    redirect(res, `/content?id=${id}`, canonicalPath);
+    return true;
+  }
+  return false;
 };
