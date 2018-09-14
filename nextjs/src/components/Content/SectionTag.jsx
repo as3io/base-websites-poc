bf5e7209891ca2df1efb87d1cbb2c1cdaa00d880
Jacob Bare <jacob@limit0.io>
2018-09-14 12:22:59 -0500

    Properly use href/as with section tag links

--- nextjs/src/components/Content/SectionTag.jsx
+++ nextjs/src/components/Content/SectionTag.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 const SectionTag = ({ alias, children, className }) => (
-  <Link href={`/section/${alias}`}>
+  <Link href={`/section?alias=${alias}`} as={`/section/${alias}`}>
     <a className={className}>{children}</a>
   </Link>
 );
 
 SectionTag.propTypes = {
   className: PropTypes.string,
-  alias: PropTypes.number.isRequired,
+  alias: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
 
