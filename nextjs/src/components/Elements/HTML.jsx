9a8dbc4828acc342271521d8af81b20fff22cea1
Jacob Bare <jacob@limit0.io>
2018-09-17 15:12:41 -0500

    Create generic Date and HTML elements

---
+++ nextjs/src/components/Elements/HTML.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import createMarkup from '../../lib/create-markup';
+
+const HTMLElement = ({
+  className,
+  collapsable,
+  html,
+  tag: Tag,
+}) => {
+  if (!html && collapsable) return null;
+  return <Tag className={className} dangerouslySetInnerHTML={createMarkup(html)} />;
+};
+
+HTMLElement.propTypes = {
+  className: PropTypes.string,
+  collapsable: PropTypes.bool,
+  html: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+HTMLElement.defaultProps = {
+  className: null,
+  collapsable: true,
+  html: '',
+  tag: 'div',
+};
+
+export default HTMLElement;
