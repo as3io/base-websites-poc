c7fd63253234bd939cd52a419cd25e978bf4c380
Jacob Bare <jacob@limit0.io>
2018-09-18 11:47:37 -0500

    Add card overlay support

--- nextjs/src/components/Templates/Content/Card/Standard.jsx
+++ nextjs/src/components/Templates/Content/Card/Standard.jsx
@@ -7,7 +7,7 @@ import DateElement from '../../../Core/Elements/Date';
 import SectionLink from '../../../Core/Elements/Content/SectionLink';
 import HTML from '../../../Core/Elements/HTML';
 
-const ContentCardStandard = ({ content, dateFormat }) => {
+const ContentCardStandard = ({ content, dateFormat, overlay }) => {
   const {
     id,
     canonicalPath,
@@ -20,6 +20,35 @@ const ContentCardStandard = ({ content, dateFormat }) => {
   const { src, alt } = primaryImage || {};
   const { name: sectionName, alias: sectionAlias } = primarySection || {};
 
+  const contents = (
+    <>
+      <Title
+        title={shortName}
+        tag="h5"
+        className="card-title"
+        contentId={id}
+        asPath={canonicalPath}
+      />
+      <HTML tag="p" className="card-text" html={teaser} />
+      <small className="card-text">
+        <SectionLink className="mr-2" sectionAlias={sectionAlias}>
+          {sectionName}
+        </SectionLink>
+        <DateElement value={published} dateFormat={dateFormat} />
+      </small>
+    </>
+  );
+
+  const elements = (
+    <div className={overlay ? 'card-img-overlay d-flex flex-column' : 'card-body'}>
+      {overlay ? (
+        <div className="mt-auto">
+          {contents}
+        </div>
+      ) : contents}
+    </div>
+  );
+
   return (
     <div className="card">
       {primaryImage && primaryImage.src && (
@@ -27,22 +56,7 @@ const ContentCardStandard = ({ content, dateFormat }) => {
           <img className="card-img-top img-fluid embed-responsive-item" src={src} alt={alt} />
         </Link>
       )}
-      <div className="card-body">
-        <Title
-          title={shortName}
-          tag="h5"
-          className="card-title"
-          contentId={id}
-          asPath={canonicalPath}
-        />
-        <HTML tag="p" className="card-text" html={teaser} />
-        <small className="card-text">
-          <SectionLink className="mr-2" sectionAlias={sectionAlias}>
-            {sectionName}
-          </SectionLink>
-          <DateElement value={published} dateFormat={dateFormat} />
-        </small>
-      </div>
+      {elements}
     </div>
   );
 };
@@ -57,11 +71,13 @@ ContentCardStandard.propTypes = {
     }),
   }),
   dateFormat: PropTypes.string,
+  overlay: PropTypes.bool,
 };
 
 ContentCardStandard.defaultProps = {
   content: {},
   dateFormat: null,
+  overlay: false,
 };
 
 export default ContentCardStandard;
