ea30c30dcdbe168481600ce48c4212984173f832
Jacob Bare <jacob@limit0.io>
2018-09-18 13:50:29 -0500

    Remove deleted section query from home page

--- nextjs/src/pages/index.jsx
+++ nextjs/src/pages/index.jsx
@@ -1,31 +1,8 @@
 import React from 'react';
-import SectionQuery from '../components/SectionQuery';
 import DefaultLayout from '../layouts/Default';
 
 export default () => (
   <DefaultLayout>
     <h1>Home</h1>
-    <SectionQuery sectionId={56155}>
-      {({ loading, error }) => {
-        if (loading) return <span>Loading...</span>;
-        if (error) {
-          return (
-            <span>
-              Error
-              {' '}
-              {error.message}
-            </span>
-          );
-        }
-
-        return (
-          <p>
-            Loaded
-            {' '}
-            56155-home
-          </p>
-        );
-      }}
-    </SectionQuery>
   </DefaultLayout>
 );
