dff545563f36d6ab42dfd3bbfa16660780d9beee
Jacob Bare <jacob@limit0.io>
2018-09-20 08:48:21 -0500

    Allow content link to accept html

--- nextjs/src/components/Core/Elements/Content/Link.jsx
+++ nextjs/src/components/Core/Elements/Content/Link.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import contentHref from '../../../../lib/content-paths';
+import createMarkup from '../../../../lib/create-markup';
 
 const ContentLinkElement = ({
   asPath,
   children,
   className,
   contentId,
+  html,
 }) => (
   <Link href={contentHref(contentId)} as={asPath}>
-    <a className={className}>{children}</a>
+    {html ? (
+      // eslint-disable-next-line react/no-danger
+      <a className={className} dangerouslySetInnerHTML={createMarkup(html)} />
+    ) : (
+      <a className={className}>{children}</a>
+    )}
   </Link>
 );
 
@@ -19,10 +26,12 @@ ContentLinkElement.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   contentId: PropTypes.number.isRequired,
+  html: PropTypes.string,
 };
 
 ContentLinkElement.defaultProps = {
   className: null,
+  html: null,
 };
 
 export default ContentLinkElement;
