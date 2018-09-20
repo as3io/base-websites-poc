453086b345ec2127c2f349acc56f48476b6c6d78
Jacob Bare <jacob@limit0.io>
2018-09-20 08:48:29 -0500

    Rename shortname to shortnamelink

---
+++ nextjs/src/components/Templates/Content/Elements/ShortNameLink.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CoreLink from '../../../Core/Elements/Content/Link';
+
+const BEM = 'content__short-name';
+
+const displayName = 'Templates/Content/Elements/ShortNameLink';
+
+const propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  canonicalPath: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  shortName: PropTypes.string.isRequired,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+const defaultProps = {
+  children: null,
+  className: null,
+  tag: 'h5',
+};
+
+const ShortNameLink = ({
+  children,
+  className,
+  canonicalPath,
+  id,
+  shortName,
+  tag: Tag,
+}) => (id && canonicalPath && (children || shortName) ? (
+  <Tag className={className ? `${BEM} ${className}` : BEM}>
+    <CoreLink
+      className="content__link"
+      contentId={id}
+      asPath={canonicalPath}
+      html={shortName}
+    >
+      {children}
+    </CoreLink>
+  </Tag>
+) : null);
+
+ShortNameLink.displayName = displayName;
+ShortNameLink.propTypes = propTypes;
+ShortNameLink.defaultProps = defaultProps;
+
+export default ShortNameLink;
