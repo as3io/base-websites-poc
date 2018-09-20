292524c613043d1ba0eb95012a237dfb4e9f8670
Jacob Bare <jacob@limit0.io>
2018-09-20 11:44:24 -0500

    Add flush property to list group

--- nextjs/src/components/Templates/Content/ListGroup.jsx
+++ nextjs/src/components/Templates/Content/ListGroup.jsx
@@ -7,20 +7,23 @@ const displayName = 'Templates/Content/ListGroup';
 const propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  flush: PropTypes.bool,
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
 const defaultProps = {
   className: null,
+  flush: false,
   tag: 'ul',
 };
 
 const ListGroup = ({
   children,
   className,
+  flush,
   tag: Tag,
 }) => (
-  <Tag className={classNames('list-group', className)}>
+  <Tag className={classNames('list-group', flush ? 'list-group-flush' : null, className)}>
     {children}
   </Tag>
 );
